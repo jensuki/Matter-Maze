38db2203cea9caf32d8322325a70dc81798900e0
jensuki <jeni.suk@proton.me>
2025-03-19 17:19:04 -0700

    basic maze setup done + added ball sprite

---
+++ index.js
@@ -0,0 +1,5 @@
+import { initMaze } from './maze.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initMaze();
+})
--- maze.js
+++ maze.js
@@ -8,12 +8,11 @@ const {
     Events
 } = Matter;
 
-// space alotted for score container
-const uiBarHeight = 50;
-
 const cellsHorizontal = 5; // # of columns
 const cellsVertical = 5; // # of rows
 
+// score container + dynamic screen dimensions
+const uiBarHeight = 50;
 const width = window.innerWidth;
 const height = window.innerHeight - uiBarHeight;
 
@@ -28,17 +27,154 @@ const engine = Engine.create();
 engine.world.gravity.y = 0;
 const { world } = engine;
 
-// create renderer for visual simulation
+// create renderer to visualize world
 const render = Render.create({
-    element: document.body, // attach to main document page
-    engine: engine, // link engine to the renderer
+    element: document.body, // attach canvas to body
+    engine: engine, // connect engine to the renderer
     options: {
         width, // width of canvas
         height // height of canvas
     }
 });
 
-// run renderer to display simulation
+// start rendering display simulation
 Render.run(render);
-// create runner using engine
 Runner.run(Runner.create(), engine);
+
+// outer bordering walls
+const outerWalls = [
+    Bodies.rectangle(width / 2, 0, width, 10, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 10, height, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 10, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 10, height, { isStatic: true })
+];
+World.add(world, outerWalls);
+
+// 2D GRID arrays (each cell intially 'unvisited' = false)
+const grid = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal).fill(false));
+
+// horizontal + vertical array (initally all 'false')
+const verticals = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal - 1).fill(false));
+const horizontals = Array(cellsVertical - 1).fill(null).map(() => Array(cellsHorizontal).fill(false));
+
+// shuffle for random path generation
+const shuffle = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        // swap
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+};
+
+/**
+ * recursive maze generation using DFS
+ * - randomly selected neighboring cell
+ * - removes walls between visited cells
+ */
+const stepThroughCell = (row, column) => {
+
+    // if current cell already visited, stop and mark as visited
+    if (grid[row][column]) return;
+    grid[row][column] = true;
+
+    // randomly order the 4 possible movement directions
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+
+    // loop through neighbors
+    for (let [nextRow, nextCol, direction] of neighbors) {
+
+        // if neighbor is out of bounds or already visited, continue
+        if (nextRow < 0
+            || nextRow >= cellsVertical
+            || nextCol < 0
+            || nextCol >= cellsHorizontal) continue;
+
+        if (grid[nextRow][nextCol]) continue;
+
+        // remove wall from horizontal or vertical neighbor depending on where we go
+        if (direction === 'up') horizontals[row - 1][column] = true;
+        else if (direction === 'right') verticals[row][column] = true;
+        else if (direction === 'down') horizontals[row][column] = true;
+        else if (direction === 'left') verticals[row][column - 1] = true;
+
+        stepThroughCell(nextRow, nextCol);
+    };
+};
+
+/**
+ * intialize and build the maze structure
+ * - recursively call stepThroughCell() to generate maze
+ * - draw vertical + horizontal walls on the canvas based on maze data
+ */
+
+const initMaze = () => {
+
+    // random starting cell row and column
+    const startRow = Math.floor(Math.random() * cellsVertical);
+    const startCol = Math.floor(Math.random() * cellsHorizontal);
+
+    stepThroughCell(startRow, startCol); // start maze generation
+
+    // draw horizontal walls
+    horizontals.forEach((row, rowIndex) => {
+        row.forEach((openColumn, columnIndex) => {
+            if (!openColumn) { // if path between rows is blocked(false), add a horizontal wall
+                World.add(world, Bodies.rectangle(
+                    (columnIndex * unitLengthX) + (unitLengthX / 2), // center x coordinate
+                    rowIndex * unitLengthY + unitLengthY, // bottom y coordinate
+                    unitLengthX, 5, {
+                    isStatic: true
+                }
+                ));
+            }
+        });
+    });
+
+    // draw vertical walls
+    verticals.forEach((column, columnIndex) => {
+        column.forEach((openRow, rowIndex) => {
+            if (!openRow) { // if path between columns is blocked(false), add a wall
+                World.add(world, Bodies.rectangle(
+                    columnIndex * unitLengthX + unitLengthX, // right side of current cell (x coordinate)
+                    (rowIndex * unitLengthY) + unitLengthY / 2, // middle of y coordinate
+                    5, unitLengthY, {
+                    isStatic: true
+                }
+                ));
+            };
+        });
+    });
+
+    // draw goal
+    World.add(world, Bodies.rectangle(
+        width - unitLengthX / 2,
+        height - unitLengthY / 2,
+        unitLengthX * .7, unitLengthY * .7, {
+        isStatic: true
+    }
+    ));
+};
+
+// ball setup
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 3;
+const ballSize = 276;
+
+const initBall = () => {
+    const ball = Bodies.circle(
+        unitLengthX / 2, unitLengthY / 2, ballRadius, {
+        render: {
+            sprite: {
+                texture: 'assets/maze_sphere.png'
+            }
+        }
+    }
+    )
+}
+
+export { initMaze };
